28eebb542ac1516a8246f3d483e389774a6a90c7
Will Maier <wcmaier@m.aier.us>
2013-04-20 22:05:14 +0000

    Us.

--- metadata.rb
+++ metadata.rb
@@ -2,5 +2,7 @@ name 'dusk'
 version '0.0.1'
 description 'Graphite/Cubism frontend'
 license 'Apache v2.0'
+maintainer 'Simple Finance'
+maintainer_email 'ops@simple.com'
 
 depends 'runit', '~> 1.1.0'
